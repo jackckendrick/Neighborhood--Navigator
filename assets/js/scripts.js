1d31a49838fdf82e70f0fdc7145b1ab979b42abc
Tshakalisa Khupe <88752230+tkhupe@users.noreply.github.com>
2023-03-22 23:45:19 -0500

    changed input colorbackground

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -220,7 +220,7 @@ document.addEventListener('DOMContentLoaded', function() {
         // $(".containerMd").addClass('hoverable');
         $(".containerLg").addClass('#82b1ff blue accent-1');
         $(".containerLg").addClass('z-depth-5 hoverable');
-        $(".input-field").addClass('#8bc34a light-green pulse');
+        $(".input-field").addClass('yellow pulse');
 
 
 
